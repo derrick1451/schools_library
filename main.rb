ee05c92b103a5b0635afb57ee1bd4d920380fbae
Derrick1451 <haylederrick14@gmail.com>
2023-08-17 11:42:59 +0300

    Delete un-wanted files

--- main.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'app'
-require_relative 'app_init'
-require_relative 'menu'
-require_relative 'data/data_manager'
-require_relative 'data/data_handler'
-
-app = App.new
-app_initializer = AppInit.new(app)
-app_initializer.start
